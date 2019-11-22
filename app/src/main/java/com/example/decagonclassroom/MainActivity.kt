6184112ae01b2baa2e1e550dd1850b5b37671c00
cchigo <ccasadu15@gmail.com>
2019-11-22 07:17:04 +0100

    create recycler view row

--- app/src/main/java/com/example/decagonclassroom/MainActivity.kt
+++ app/src/main/java/com/example/decagonclassroom/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.decagonclassroom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+abstract class MainActivity : AppCompatActivity() ,BottomNavigationView.OnNavigationItemReselectedListener {
+
+    lateinit var bottomNavigationView : BottomNavigationView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation_view)
+
+
+
+
     }
+
+
 }
