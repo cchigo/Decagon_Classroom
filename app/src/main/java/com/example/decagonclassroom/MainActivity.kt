ff3f48284d3898e5a8257fe653bcf1a335efe0fb
cchigo <ccasadu15@gmail.com>
2019-11-22 11:30:00 +0100

    set on click listeners for bottom navigation

--- app/src/main/java/com/example/decagonclassroom/MainActivity.kt
+++ app/src/main/java/com/example/decagonclassroom/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.decagonclassroom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import com.example.decagonclassroom.R.id.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
-abstract class MainActivity : AppCompatActivity() ,BottomNavigationView.OnNavigationItemReselectedListener {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+
 
     lateinit var bottomNavigationView : BottomNavigationView
 
@@ -14,12 +18,58 @@ abstract class MainActivity : AppCompatActivity() ,BottomNavigationView.OnNaviga
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        bottomNavigationView = findViewById(R.id.bottom_navigation_view)
+      bottomNavigationView = findViewById(R.id.bottom_navigation_view)
 
+      bottomNavigationView.selectedItemId = R.id.navigation_curriculum
 
-
+       bottomNavigationView.setOnNavigationItemSelectedListener(this)
 
     }
+
+    var home =  HomeFragment()
+    var curriculum = CurriculumFragment()
+    var resources = ResourcesFragment()
+    var profile = ProfileFragment()
+
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+
+       when (p0.getItemId()){
+           navigation_home ->{
+           getSupportFragmentManager()
+               .beginTransaction()
+               .replace(R.id.container, home).commit()
+                return true
+           }
+           navigation_curriculum ->{
+               getSupportFragmentManager()
+                   .beginTransaction()
+                   .replace(R.id.container, curriculum).commit()
+               return true
+           }
+           navigation_resources ->{
+               getSupportFragmentManager()
+                   .beginTransaction()
+                   .replace(R.id.container, resources).commit()
+               return true
+           }
+           navigation_profile ->{
+               getSupportFragmentManager()
+                   .beginTransaction()
+                   .replace(R.id.container, profile).commit()
+               return true
+           }
+       }
+
+        return true
+    }
+
+
+
+
+
+
+
 
 
 }
