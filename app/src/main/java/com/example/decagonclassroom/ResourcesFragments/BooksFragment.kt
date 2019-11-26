623bd1ffe6e733ff83b73ed9008e2b03b2744f3c
cchigo <ccasadu15@gmail.com>
2019-11-27 00:48:53 +0100

    fix resources page

--- app/src/main/java/com/example/decagonclassroom/ResourcesFragments/BooksFragment.kt
+++ app/src/main/java/com/example/decagonclassroom/ResourcesFragments/BooksFragment.kt
@@ -1,4 +1,4 @@
-package com.example.decagonclassroom
+package com.example.decagonclassroom.ResourcesFragments
 
 
 import android.os.Bundle
@@ -7,17 +7,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
+import com.example.decagonclassroom.R
+
 /**
  * A simple [Fragment] subclass.
  */
-class ArticlesFragment : Fragment() {
+class BooksFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_articles, container, false)
+        return inflater.inflate(R.layout.fragment_books, container, false)
     }
 
 
